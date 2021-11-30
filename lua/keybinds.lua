9db848306e971a4905e07c4b2888925e330b5b8e
David O'Rourke <david.orourke@gmail.com>
2021-11-30 14:48:18 +0000

    keybinds: Add C-BS to close buffer

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -3,6 +3,15 @@ local util = require "util"
 
 util.inoremap("<C-U>", "<C-G>u<C-U>")
 util.map("Q", "gq")
+
+-- Ctrl-Backspace closes the buffer
+util.nmap("", ":bdelete<CR>")
+
+-- Toggle line numbers
 util.nmap("<F11>", ":set invnumber<CR> :set list!<CR>")
+
+-- Previous buffer
 util.nnoremap("<C-Left>", ":bprevious<CR>")
+
+-- Next buffer
 util.nnoremap("<C-Right>", ":bnext<CR>")
