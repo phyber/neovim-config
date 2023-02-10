63f4fcd12706115fa6e1c728afd45560714fb86a
David O'Rourke <david.orourke@gmail.com>
2023-02-10 13:09:03 +0000

    git: Allow passing extra options

--- lua/git.lua
+++ lua/git.lua
@@ -6,19 +6,20 @@ local git = {}
 
 git.cmds = {
     git = "git",
-    clone = "git clone %s %s",
+    clone = "git clone %s %s %s",
 }
 
 -- Clones a git repo from repo to dest
 -- Returns false on failure, true on success
-function git:clone(repo, dest)
+function git:clone(repo, dest, options)
     if not util.is_executable(self.cmds.git) then
         print("git not found, cannot continue")
 
         return false
     end
 
-    fn.system(self.cmds.clone:format(repo, dest))
+    local args = table.concat(options or {}, " ")
+    fn.system(self.cmds.clone:format(repo, args, dest))
 
     return util.is_exit_success()
 end
