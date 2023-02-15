ccab87b6d0a35f3d59eeccc7425ed550590a3f9d
David O'Rourke <david.orourke@gmail.com>
2023-02-15 12:41:02 +0000

    git: Improve handling of cmds

--- lua/git.lua
+++ lua/git.lua
@@ -4,7 +4,7 @@ local fn = vim.fn
 
 local git = {}
 
-git.cmds = {
+local cmds = {
     git = "git",
     clone = "git clone %s %s %s",
 }
@@ -12,14 +12,14 @@ git.cmds = {
 -- Clones a git repo from repo to dest
 -- Returns false on failure, true on success
 function git:clone(repo, dest, options)
-    if not util.is_executable(self.cmds.git) then
+    if not util.is_executable(cmds.git) then
         print("git not found, cannot continue")
 
         return false
     end
 
     local args = table.concat(options or {}, " ")
-    fn.system(self.cmds.clone:format(repo, args, dest))
+    fn.system(cmds.clone:format(repo, args, dest))
 
     return util.is_exit_success()
 end
