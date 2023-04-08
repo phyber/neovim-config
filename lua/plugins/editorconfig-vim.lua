d64fa5f133a9b7a3bda6df62b2fd94e87b3ce0e8
David O'Rourke <david.orourke@gmail.com>
2023-04-08 10:33:01 +0100

    plugins: Only load editorconfig on versions without 0.9 features

--- lua/plugins/editorconfig-vim.lua
+++ lua/plugins/editorconfig-vim.lua
@@ -1,6 +1,12 @@
 -- Editorconfig (https://editorconfig.org)
 local plugin = {
     "editorconfig/editorconfig-vim",
+    cond = function()
+        -- Neovim 0.9+ include editorconfig functionality by default
+        local util = require("util")
+
+        return not util.nvim_has("nvim-0.9")
+    end,
 }
 
 return plugin
