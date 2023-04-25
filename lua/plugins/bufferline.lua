2450bacdd53befbf914a5a9b1154584b322ea5c4
David O'Rourke <david.orourke@gmail.com>
2023-04-25 19:34:51 +0100

    plugins: Take care of a bufferline deprecation

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -10,14 +10,22 @@ local plugin = {
                 numbers = "ordinal",
                 show_buffer_icons = false,
                 show_buffer_close_icons = false,
-                show_buffer_default_icon = false,
                 show_close_icon = false,
 
                 indicator = {
                     style = "none",
                 },
 
-                separator_style = {'|', '|'},
+                separator_style = {
+                    '|',
+                    '|',
+                },
+
+                -- show_buffer_default_icon was deprecated. Maybe this is
+                -- equivalent, who knows?
+                get_element_icon = function(_bufnr)
+                    return false
+                end,
             },
         })
     end,
