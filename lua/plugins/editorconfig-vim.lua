d687f8331d8dc308892e867cc4f481cbe10b2b5b
David O'Rourke <david.orourke@gmail.com>
2023-05-01 00:10:15 +0100

    plugins: Unify how we check for nvim versions

--- lua/plugins/editorconfig-vim.lua
+++ lua/plugins/editorconfig-vim.lua
@@ -3,9 +3,7 @@ local plugin = {
     "editorconfig/editorconfig-vim",
     cond = function()
         -- Neovim 0.9+ include editorconfig functionality by default
-        local util = require("util")
-
-        return not util.nvim_has("nvim-0.9")
+        return not require("util").nvim_has("nvim-0.9")
     end,
 }
 
