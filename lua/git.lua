37357cb9b91a2aa1eabbe8044de3bd86f5e9eb01
David O'Rourke <david.orourke@gmail.com>
2023-05-17 17:45:59 +0100

    git: Execute directly instead of via the shell

--- lua/git.lua
+++ lua/git.lua
@@ -1,22 +1,32 @@
 -- Git module
 local filesystem = require "filesystem"
 local util = require "util"
+local flatten = vim.fn.flatten
 local system = vim.fn.system
 
+local CLONE = "clone"
 local GIT = "git"
-local GIT_CLONE = "git clone %s %s %s"
 
 -- Clones a git repo from repo to dest
 -- Returns false on failure, true on success
-local function clone(repo, dest, options)
+local function clone(repository, destination, options)
     if not filesystem.is_executable(GIT) then
         print("git not found, cannot continue")
 
         return false
     end
 
-    local args = table.concat(options or {}, " ")
-    system(GIT_CLONE:format(repo, args, dest))
+    local args = options or {}
+
+    local cmd = {
+        GIT,
+        CLONE,
+        repository,
+        args,
+        destination,
+    }
+
+    system(flatten(cmd))
 
     return util.is_exit_success()
 end
